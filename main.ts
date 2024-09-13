a349b4a0541d427745078c36f8e5db854786eb3b
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-13 12:07:06 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.pause(2000)
+    FAmpel(0, 1)
+    basic.pause(7000)
+    FAmpel(1, 0)
 })
+function FAmpel (Red: number, Green: number) {
+    pins.digitalWritePin(DigitalPin.P1, Red)
+    pins.digitalWritePin(DigitalPin.P2, Green)
+    if (Red == 1) {
+        basic.setLedColor(0xff0000)
+    } else if (Green == 1) {
+        basic.setLedColor(0x00ff00)
+    } else {
+        basic.turnRgbLedOff()
+    }
+}
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+    basic.pause(2000)
+    FAmpel(0, 1)
+    basic.pause(7000)
+    FAmpel(1, 0)
+})
+basic.showString("01AMPEL")
+FAmpel(1, 1)
